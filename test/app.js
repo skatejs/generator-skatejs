d0d1bcc1dc55d4765e8f5fdd780d6869e9b5906d
Alex LaFroscia <alexlafroscia@nestlabs.com>
2017-02-24 13:06:07 -0800

    Add tests for existing functionality

--- test/app.js
+++ test/app.js
@@ -4,15 +4,28 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-skatejs:app', function () {
-  before(function () {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
-      .toPromise();
-  });
+  describe('creating the initial component', function() {
+    it('creates a component with the correct name', function() {
+      return helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts({ initialComponentName: 'x-foo' })
+        .toPromise()
+        .then(function() {
+          // Make sure that the initial component was created
+          assert.file('src/components/x-foo/component.js');
+          assert.file('src/components/x-foo/styles.scss');
+        });
+    });
 
-  it('creates files', function () {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+    it.skip('ensures that a default component name is valid', function() {
+      return helpers.run(path.join(__dirname, '../generators/app'))
+        .inTmpDir(function() {
+          // Do something to set the appname for this run
+        })
+        .toPromise()
+        .then(function() {
+          assert.file('src/components/x-foo/component.js');
+          assert.noFile('src/components/foo/component.js');
+        });
+    });
   });
 });
--- test/component.js
+++ test/component.js
@@ -4,15 +4,39 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-skatejs:component', function () {
-  before(function () {
-    return helpers.run(path.join(__dirname, '../generators/component'))
-      .withPrompts({someAnswer: true})
-      .toPromise();
+  describe('generating the component structure', function() {
+    it('prompts for a component name if not given one', function() {
+      return helpers.run(path.join(__dirname, '../generators/component'))
+        .withPrompts({ componentName: 'x-foo' })
+        .toPromise()
+        .then(function() {
+          // Make sure that the initial component was created
+          assert.file('src/components/x-foo/component.js');
+          assert.file('src/components/x-foo/styles.scss');
+        });
+    });
+
+    it('can be given a component name to generate', function() {
+      return helpers.run(path.join(__dirname, '../generators/component'))
+        .withArguments([ 'x-foo' ])
+        .toPromise()
+        .then(function() {
+          // Make sure that the initial component was created
+          assert.file('src/components/x-foo/component.js');
+          assert.file('src/components/x-foo/styles.scss');
+        });
+    });
   });
 
-  it('creates files', function () {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+  describe('configuring the generated component', function() {
+    it('registers the component with the correct name', function() {
+      return helpers.run(path.join(__dirname, '../generators/component'))
+        .withPrompts({ componentName: 'x-foo' })
+        .toPromise()
+        .then(function() {
+          assert.fileContent('src/components/x-foo/component.js', /return 'x-foo';/);
+          assert.fileContent('src/components/x-foo/component.js', /Hello, world! I am x-foo!/);
+        });
+    });
   });
 });
