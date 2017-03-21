d7ead60bf20171e32a6b216fa78bc514566b3f07
Alex LaFroscia <alexlafroscia@nestlabs.com>
2017-03-20 18:13:32 -0700

    Run ESLint outside of Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,11 @@
 'use strict';
 var path = require('path');
 var gulp = require('gulp');
-var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
-
-gulp.task('static', function () {
-  return gulp.src(['**/*.js', '!**/templates/**/*.js'])
-    .pipe(excludeGitignore())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
 
 gulp.task('nsp', function (cb) {
   nsp({package: path.resolve('package.json')}, cb);
@@ -49,4 +40,4 @@ gulp.task('watch', function () {
 });
 
 gulp.task('prepublish', ['nsp']);
-gulp.task('default', ['static', 'test']);
+gulp.task('default', ['test']);
